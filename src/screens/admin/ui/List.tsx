d59a394d9539361b0396e7b9573ee27ca5d938fd
ShotMeow <shotmeow@gmail.com>
2024-05-19 09:32:43 +0300

    Added: Admin CRUD

--- src/screens/admin/ui/List.tsx
+++ src/screens/admin/ui/List.tsx
@@ -1,10 +1,10 @@
 import type { FC } from "react";
-import TagsList from "../lists/TagsList";
-import StreamsList from "../lists/StreamsList";
-import ProductsList from "../lists/ProductsList";
-import NewsList from "../lists/NewsList";
-import TeamsList from "../lists/TeamsList";
-import TournamentsList from "../lists/TournamentsList";
+import TagsList from "./lists/TagsList";
+import StreamsList from "./lists/StreamsList";
+import ProductsList from "./lists/ProductsList";
+import NewsList from "./lists/NewsList";
+import TeamsList from "./lists/TeamsList";
+import TournamentsList from "./lists/TournamentsList";
 
 interface Props {
   type: string;
